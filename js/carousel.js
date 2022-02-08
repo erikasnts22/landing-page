6142e502d86df0257a1cfd487727c8755359561f
erikasnts22 <sntserika22@gmail.com>
2022-02-09 05:57:12 +0800

    fixed carousel images

--- js/carousel.js
+++ js/carousel.js
@@ -1,28 +1,27 @@
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+let slider = tns({
+  container : ".my-slider",
+  "slideBy": "1",
+  "speed" : 400,
+  "nav" : false,
+  autoplay : true,
+  controls : false,
+  autoplayButtonOutput : false,
+  responsive : {
+      1600: {
+          items : 3,
+          gutter : 20
+      },
+      1024: {
+          items : 3,
+          gutter : 20
+      },
+      768: {
+          items : 3,
+          gutter : 20
+      },
+      480: {
+          items : 1,
+          gutter : 20
+      }
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+})
